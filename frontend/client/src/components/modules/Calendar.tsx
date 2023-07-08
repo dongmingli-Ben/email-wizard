a9864b223bfa6d3dd67fe2cf5b4a1c9cecd37863
ben <dongmingli_Ben@126.com>
2023-07-08 21:38:32 +0800

    update calendar skeleton

--- frontend/client/src/components/modules/Calendar.tsx
+++ frontend/client/src/components/modules/Calendar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 
@@ -7,17 +7,38 @@ type calendarProps = {
   userSecret: string;
 };
 
+type EventType = {
+  title: string;
+  date: string;
+};
+
+const EVENTS = [
+  { title: "event 1", date: "2023-07-01" },
+  { title: "event 2", date: "2023-04-02" },
+];
+
+const getAPIEvents = async (userId: string, userSecret: string) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return EVENTS;
+};
+
 const Calendar = (props: calendarProps) => {
+  const [events, setEvents] = useState<EventType[]>([]);
+
+  useEffect(() => {
+    getAPIEvents(props.userId, props.userSecret).then(
+      (_events: EventType[]) => {
+        setEvents(_events);
+      }
+    );
+  }, []);
   return (
     <div className="calendar-container u-block">
       <FullCalendar
         plugins={[dayGridPlugin]}
         initialView="dayGridMonth"
         weekends={true}
-        events={[
-          { title: "event 1", date: "2023-07-01" },
-          { title: "event 2", date: "2023-04-02" },
-        ]}
+        events={events}
       />
     </div>
   );
