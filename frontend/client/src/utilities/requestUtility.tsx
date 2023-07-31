d0279cbfdfe30936a9d76cd06cbbc8a34a9e7345
ben <dongmingli_Ben@126.com>
2023-07-31 22:32:00 +0800

    update get and post to axios

--- frontend/client/src/utilities/requestUtility.tsx
+++ frontend/client/src/utilities/requestUtility.tsx
@@ -7,71 +7,44 @@
  * These functions return promises, which means you should use ".then" on them.
  * e.g. get('/api/foo', { bar: 0 }).then(res => console.log(res))
  */
-
-// ex: formatParams({ some_key: "some_value", a: "b"}) => "some_key=some_value&a=b"
-function formatParams(params: Object): string {
-  // iterate of all the keys of params as an array,
-  // map it to a new array of URL string encoded key,value pairs
-  // join all the url params using an ampersand (&).
-  return Object.keys(params)
-    .map((key) => key + "=" + encodeURIComponent(params[key]))
-    .join("&");
-}
-
-// convert a fetch result to a JSON object with error handling for fetch and json errors
-async function convertToJSON(res: Response) {
-  if (!res.ok) {
-    console.log(
-      `API request failed with response status ${res.status} and text: ${res.statusText}`
-    );
-    let errData = await res.json();
-    throw new Error(errData.errMsg);
-  }
-
-  return res
-    .clone() // clone so that the original is still readable for debugging
-    .json() // start converting to JSON object
-    .catch((error) => {
-      // throw an error containing the text that couldn't be converted to JSON
-      return res.text().then((text) => {
-        throw `API request's result could not be converted to a JSON object: \n${text}`;
-      });
-    });
-}
+import axios, { AxiosResponse, AxiosError } from "axios";
 
 // Helper code to make a get request. Default parameter of empty JSON Object for params.
 // Returns a Promise to a JSON Object.
 export async function get(endpoint: string, params = {}) {
-  const fullPath = endpoint + "?" + formatParams(params);
-  try {
-    const res = await fetch(fullPath, {
-      method: "GET",
+  return axios
+    .get(endpoint, {
+      params: params,
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET,POST,OPTIONS,DELETE,PUT",
       },
+    })
+    .then((resp: AxiosResponse) => {
+      // console.log(resp);
+      return resp.data;
+    })
+    .catch((error: AxiosError) => {
+      console.log(error.response?.data);
+      throw `GET request to ${endpoint} failed with error:\n${error}`;
     });
-    return convertToJSON(res);
-  } catch (error) {
-    // give a useful error message
-    throw `GET request to ${fullPath} failed with error:\n${error}`;
-  }
 }
 
 // Helper code to make a post request. Default parameter of empty JSON Object for params.
 // Returns a Promise to a JSON Object.
 export async function post(endpoint: string, params = {}) {
-  try {
-    const res = await fetch(endpoint, {
-      method: "post",
+  return axios
+    .post(endpoint, params, {
       headers: { "Content-type": "application/json" },
-      body: JSON.stringify(params),
+    })
+    .then((resp: AxiosResponse) => {
+      console.log(resp);
+      return resp.data;
+    })
+    .catch((error) => {
+      console.log(error.response?.data);
+      throw `POST request to ${endpoint} failed with error:\n${error}`;
     });
-    return convertToJSON(res);
-  } catch (error) {
-    // give a useful error message
-    throw `POST request to ${endpoint} failed with error:\n${error}`;
-  }
 }
 
 export const backendConfig = {
