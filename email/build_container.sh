8230938092ca0afae1e2bb68b5c8ab4a2ee65fa9
ben <dongmingli_Ben@126.com>
2023-10-16 08:03:54 +0800

    update email container for gmail

--- email/build_container.sh
+++ email/build_container.sh
@@ -1,3 +1,3 @@
 code_dir="$(cd "$(dirname "$0")" && cd .. && pwd)"
 
-docker run -it --net=host --ipc=host --privileged=true -v ${code_dir}:/mnt --name email pyemail:v0.1 /bin/bash
+docker run -it --net=host --ipc=host --privileged=true -v ${code_dir}:/mnt --name email pyemail:v0.2 /bin/bash
